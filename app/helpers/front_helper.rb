ddabf55f9d31f6e908e6b0de84ed1f389bac80af
sue445 <sue445@sue445.net>
2015-01-28 01:20:44 +0900

    padrino g controller Front

---
+++ app/helpers/front_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Sebastian
+  class App
+    module FrontHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers FrontHelper
+  end
+end
---
+++ spec/app/helpers/front_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe "Sebastian::App::FrontHelper" do
+  pending "add some examples to (or delete) #{__FILE__}" do
+    let(:helpers){ Class.new }
+    before { helpers.extend Sebastian::App::FrontHelper }
+    subject { helpers }
+
+    it "should return nil" do
+      expect(subject.foo).to be_nil
+    end
+  end
+end
