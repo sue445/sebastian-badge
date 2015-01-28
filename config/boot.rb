43a952b61608d4d044f0b8a70dde87e480e07d10
sue445 <sue445@sue445.net>
2015-01-29 01:37:40 +0900

    Tweak logging

--- config/boot.rb
+++ config/boot.rb
@@ -31,6 +31,11 @@ Bundler.require(:default, RACK_ENV)
 #   include Padrino::Helpers::TranslationHelpers
 # end
 
+Padrino::Logger::Config[:production][:stream] = :stdout
+Padrino::Logger::Config[:production][:log_level] = :info
+Padrino::Logger::Config[:test][:stream] = :stdout
+Padrino::Logger::Config[:test][:log_level] = :info
+
 ##
 # Add your before (RE)load hooks here
 #
