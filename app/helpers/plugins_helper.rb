6de8d718e9417045bef037d2082536f2b5c6cd88
sue445 <sue445@sue445.net>
2015-01-30 00:29:20 +0900

    padrino g controller Plugins get:index get:show

---
+++ app/helpers/plugins_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Sebastian
+  class App
+    module PluginsHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers PluginsHelper
+  end
+end
