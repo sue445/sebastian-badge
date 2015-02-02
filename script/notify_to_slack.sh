1fcbef93971661d4614534dae4c533af855063d7
sue445 <sue445@sue445.net>
2015-02-03 08:35:12 +0900

    post to slack only exists SLACK_TOKEN

--- script/notify_to_slack.sh
+++ script/notify_to_slack.sh
@@ -3,4 +3,6 @@
 NAME=$1
 MESSAGE=$2
 
-curl https://slack.com/api/chat.postMessage -X POST -d 'channel=#sebastian' -d "text=${MESSAGE}" -d "username=${NAME}" -d "token=${SLACK_TOKEN}"
+if [ -n "${SLACK_TOKEN}" ]; then
+  curl https://slack.com/api/chat.postMessage -X POST -d 'channel=#sebastian' -d "text=${MESSAGE}" -d "username=${NAME}" -d "token=${SLACK_TOKEN}"
+fi
