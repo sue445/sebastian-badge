e4c9b50cf9bb16f74328e7f5c09347484f9dd3b1
sue445 <sue445@sue445.net>
2018-08-17 23:23:41 +0900

    Cosmetic change :lipstick:

--- spec/factories/plugins.rb
+++ spec/factories/plugins.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :plugin do
     name        { Faker::Precure.user_name }
     title       { Faker::Precure.title }
-    version { "0.0.1" }
+    version     { "0.0.1" }
     wiki_url    { "http://#{Faker::Precure.user_name}.com" }
     released_at { Time.current }
   end
