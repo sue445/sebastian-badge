beaaa11eceae06c0a84427e8373499ccdb2c1d19
sue445 <sue445@sue445.net>
2021-09-27 22:59:53 +0900

    Migrate to Heroku orb

--- .circleci/heroku_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-heroku config:add BUNDLE_WITHOUT="test:development" --app $HEROKU_APP_NAME
-
-# git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:master
-git push git@heroku.com:$HEROKU_APP_NAME.git master:master
